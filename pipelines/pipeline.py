d286aeebd67973d429ece239a26de79713e0c1b4
Vamshi Krishna S <107762231+VamshiKrsna@users.noreply.github.com>
2024-09-20 22:16:16 +0530

    added general pipeline

--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -1,12 +1,11 @@
-# from steps.fred_data_ingestion import ingest_fred_data
 import sys
 import os
-root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(str(root))
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
 
+from steps.model_builder_evaluator import ConcreteBestModelFinder
 from steps.data_loader_splitter import DataLoaderStrategy, DataDivideStrategy
-from steps.model_builder_evaluator import ConcreteBestModelFinder 
 
-df = DataLoaderStrategy().handle_data("../data/Final.csv")  
+df = DataLoaderStrategy().handle_data("../data/Final.csv")
 best_model = ConcreteBestModelFinder().find_best_model(df)
 print(f"The Best Model Is : {type(best_model).__name__}")
